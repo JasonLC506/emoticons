9bc067cb5b78f2467a605fac947fba27fe6aa7fb
JasonLC506 <jpz5181@ist.psu.edu>
2016-10-27 13:14:41 -0400

    compare recall and constraint function in test.py on going

--- test.py
+++ test.py
@@ -2,8 +2,20 @@ import numpy as np
 from logRegFeatureEmotion import *
 from sklearn.model_selection import KFold
 import itertools
+import math
 
 
-a = {1:2, 2:3, 3:4}
-b = a.keys()
-b
+NCLASS = 6
+NRANK = 6
+
+def nConstraints():
+    N_per = math.factorial(NCLASS)/math.factorial(NCLASS-NRANK)
+    N_constr = [0 for i in range(N_per)]
+    rank_per = rankPer()
+
+def rankPer():
+    N_per = math.factorial(NCLASS) / math.factorial(NCLASS - NRANK)
+    rank_per = [[] for i in range(N_per)]
+    
+
+
