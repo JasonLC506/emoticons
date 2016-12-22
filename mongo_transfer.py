c72dc7c3760c8bb9dfc7fe4194f04359e9db1942
Jiasheng Zhang <jpz5181@lws-dongwon3.ist.psu.edu>
2016-12-22 18:50:38 -0500

    washington emotion finished

--- mongo_transfer.py
+++ mongo_transfer.py
@@ -5,11 +5,11 @@ ssh jiasheng@191.234.186.42 -L 27018:localhost:27017
 """
 
 db_name = "dataSet"
-collection_names = []
+collection_names = ["reactions_collection_nytimes_final"]
 
 client = pymongo.MongoClient()
 db = client[db_name]
 for collection_name in collection_names:
-    db.command("cloneCollection", **{"cloneCollection":db_name+collection_name,"collection":db_name+"."+collection_name, "from":"localhost:27018"})
+    db.command("cloneCollection", **{"cloneCollection":db_name+"."+collection_name,"collection":db_name+"."+collection_name, "from":"localhost:27018"})
     print db[collection_name].count()
 
