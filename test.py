8db1c19df6fdc1962107bd166cf8613a45cb8ea3
JasonLC506 <jpz5181@ist.psu.edu>
2017-04-12 12:07:20 -0400

    KNNParallel, current PlackettLuce, can be changed to Mallows

--- test.py
+++ test.py
@@ -10,45 +10,48 @@ import DecisionTreeWeight_Bordar as dtb
 from readSushiData import readSushiData
 
 
-def cumulate(y, L, K):
-    x = np.zeros(L*K, dtype=np.float16).reshape([L, K])
-    for i in range(L):
-        x[i] = np.sum(y[:i, :], axis=0)
-    return x
+# def cumulate(y, L, K):
+#     x = np.zeros(L*K, dtype=np.float16).reshape([L, K])
+#     for i in range(L):
+#         x[i] = np.sum(y[:i, :], axis=0)
+#     return x
 
-x, y = readSushiData()
+# x, y = readSushiData()
 
 # m = np.array([9,2,1]).reshape([1,3])
 # print np.repeat(m, 3, axis=0)
 
 
 
-def crossValidateTest(x, y, cv=5, alpha=0.0, rank_weight=False, stop_criterion_mis_rate=None, stop_criterion_min_node=1,
-                  stop_criterion_gain=0.0, prune_criteria=0):
+# def crossValidateTest(x, y, cv=5, alpha=0.0, rank_weight=False, stop_criterion_mis_rate=None, stop_criterion_min_node=1,
+#                   stop_criterion_gain=0.0, prune_criteria=0):
+#
+#
+#     results = {"perf": []}
+#
+#     # cross validation #
+#     np.random.seed(1100)
+#     kf = KFold(n_splits=cv, shuffle=True, random_state=0)  ## for testing fixing random_state
+#     for train, test in kf.split(x):
+#         x_train = x[train, :]
+#         y_train = y[train, :]
+#         x_test = x[test, :]
+#         y_test = y[test, :]
+#
+#         y_pred_single = dtb.DecisionTree().nodeResult(y_train,None)
+#         print "simple Bordar aggregation result: ", y_pred_single
+#         y_pred = np.repeat(y_pred_single.reshape([1,y_pred_single.shape[0]]), y_test.shape[0], axis=0)
+#         results["perf"].append(LogR.perfMeasure(y_pred, y_test, rankopt=True))
+#
+#     for key in results.keys():
+#         item = np.array(results[key])
+#         mean = np.nanmean(item, axis=0)
+#         std = np.nanstd(item, axis=0)
+#         results[key] = [mean, std]
+#
+#     return results
+#
+# print crossValidateTest(x,y)
 
-
-    results = {"perf": []}
-
-    # cross validation #
-    np.random.seed(1100)
-    kf = KFold(n_splits=cv, shuffle=True, random_state=0)  ## for testing fixing random_state
-    for train, test in kf.split(x):
-        x_train = x[train, :]
-        y_train = y[train, :]
-        x_test = x[test, :]
-        y_test = y[test, :]
-
-        y_pred_single = dtb.DecisionTree().nodeResult(y_train,None)
-        print "simple Bordar aggregation result: ", y_pred_single
-        y_pred = np.repeat(y_pred_single.reshape([1,y_pred_single.shape[0]]), y_test.shape[0], axis=0)
-        results["perf"].append(LogR.perfMeasure(y_pred, y_test, rankopt=True))
-
-    for key in results.keys():
-        item = np.array(results[key])
-        mean = np.nanmean(item, axis=0)
-        std = np.nanstd(item, axis=0)
-        results[key] = [mean, std]
-
-    return results
-
-print crossValidateTest(x,y)
+x = np.array([1,2,3,4])
+print x[2:5]
