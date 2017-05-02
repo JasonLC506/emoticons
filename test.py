bedafe5e799e4ae1933e4d5b986bec3c069e9558
JasonLC506 <jpz5181@ist.psu.edu>
2017-05-02 12:19:25 -0400

    crawlposts crawling early post ids

--- test.py
+++ test.py
@@ -14,29 +14,8 @@ from scipy.optimize import curve_fit
 from mpl_toolkits.mplot3d import Axes3D
 from matplotlib import cm
 from matplotlib.ticker import LinearLocator, FormatStrFormatter
+import time
 
-
-
-fig = plt.figure()
-ax = fig.gca(projection='3d')
-
-# Make data.
-X = np.arange(-5, 5, 0.25)
-Y = np.arange(-5, 5, 0.25)
-X, Y = np.meshgrid(X, Y)
-R = np.sqrt(X**2 + Y**2)
-Z = np.sin(R)
-
-# Plot the surface.
-surf = ax.plot_surface(X, Y, Z, cmap=cm.coolwarm,
-                       linewidth=0, antialiased=False)
-
-# Customize the z axis.
-# ax.set_zlim(-1.01, 1.01)
-# ax.zaxis.set_major_locator(LinearLocator(10))
-# ax.zaxis.set_major_formatter(FormatStrFormatter('%.02f'))
-
-# Add a color bar which maps values to colors.
-# fig.colorbar(surf, shrink=0.5, aspect=5)
-
-plt.show()
+ctime = time.strptime("2017-05-02T15:28:00+0000", "%Y-%m-%dT%H:%M:%S+0000")
+ntime = time.strptime("2015-05-02T15:28:00+0000", "%Y-%m-%dT%H:%M:%S+0000")
+print ntime > ctime
