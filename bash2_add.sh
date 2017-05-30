4eec29dcef2b0dd142bc9f9be14ae62bd3654cc0
jasonlc506 <jpz5181@ist.psu.edu>
2017-05-30 03:26:17 +0000

    SMPrank synthetic data third run

--- bash2_add.sh
+++ bash2_add.sh
@@ -9,7 +9,7 @@ for((i=0;i<${#par_a[@]};i++))
      #b=${par_b[$j]}
      echo $a
      #echo $b
-     nohup python logLinear.py $a >> logs/log_logLinear.py_${a} &
+     nohup python SMPrank.py $a >> logs/log_SMPrank.py_${a} &
   #}
 }
 exit 0
